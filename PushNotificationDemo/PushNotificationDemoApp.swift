7ea5284cb7bd568006568809c558ad884f51b09f
Erik Flores <erikfloresq@icloud.com>
2023-03-04 14:48:36 -0500

    Initial Commit

---
+++ PushNotificationDemo/PushNotificationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationDemoApp.swift
+//  PushNotificationDemo
+//
+//  Created by Erik Flores on 4/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
